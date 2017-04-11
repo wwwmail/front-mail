a93bf9819cc7d024303a64f0f9076ffae9732f99
iloveall <05046800>
2017-04-11 20:00:44 +0300

    Added show message

--- src/client/app/mail/message/message.controller.js
+++ src/client/app/mail/message/message.controller.js
@@ -5,15 +5,27 @@
         .module('mail.message')
         .controller('MessageController', MessageController);
 
-    MessageController.$inject = ['$state'];
+    MessageController.$inject = ['mail', '$state'];
     /* @ngInject */
-    function MessageController($state) {
+    function MessageController(mail, $state) {
         var vm = this;
+
+        vm.message = {};
 
         activate();
 
         function activate() {
             // alert($state.params.id);
+            getMessage();
+        }
+
+        function getMessage() {
+            // console.log('get', message);
+            mail.getById({id: $state.params.id}).then(function (response) {
+                vm.message.model = response;
+
+                console.log('message', vm.message.model);
+            });
         }
     }
 })();
