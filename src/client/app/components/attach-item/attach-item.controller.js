f094855fd4bd27219be9d2d72a06ffc6e0e42270
Aleksand Semenov <lovealldevelop@gmail.com>
2017-05-11 18:39:38 +0300

    Added stage commit

--- src/client/app/components/attach-item/attach-item.controller.js
+++ src/client/app/components/attach-item/attach-item.controller.js
@@ -5,15 +5,17 @@
         .module('app.components')
         .controller('AttachItemController', AttachItemController);
 
-    AttachItemController.$inject = ['$scope'];
+    AttachItemController.$inject = ['$auth'];
     /* @ngInject */
-    function AttachItemController($scope) {
+    function AttachItemController($auth) {
         var vm = this;
 
         activate();
 
         function activate() {
             console.log('attach', vm.attach);
+
+            vm.user = $auth.user;
         }
     }
 })();
