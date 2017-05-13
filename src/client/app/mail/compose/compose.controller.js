ca48747087c82873db57613b744698f4b0bafc63
Aleksandr Semenov <lovealldevelop@gmail.com>
2017-05-13 19:04:04 +0300

    Stage commit

--- src/client/app/mail/compose/compose.controller.js
+++ src/client/app/mail/compose/compose.controller.js
@@ -158,5 +158,26 @@
 
             return to;
         }
+
+        function upload(dataUrl, name) {
+            var blob = Upload.dataUrltoBlob(dataUrl, name);
+
+            vm.avatar.upload = profile.uploadAvatar({imageFile: blob});
+
+            vm.avatar.isLoading = true;
+            vm.avatar.upload.then(function (response) {
+                $timeout(function () {
+                    vm.avatar.isLoading = false;
+                    close();
+                });
+            }, function (response) {
+                // toastr.success('Не удалось загрузить аватар, пожалуйста, повторите попытку', 'Ошибка');
+            }, function (evt) {
+                // file.progress = Math.min(100, parseInt(100.0 *
+                //                          evt.loaded / evt.total));
+            });
+        }
+
+
     }
 })();
