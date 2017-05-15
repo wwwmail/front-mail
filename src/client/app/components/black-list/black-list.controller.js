9771ca4e0369c961ba7811207e8d74854ef9313d
Aleksand Semenov <lovealldevelop@gmail.com>
2017-05-15 22:09:15 +0300

    Added black-list

--- src/client/app/components/black-list/black-list.controller.js
+++ src/client/app/components/black-list/black-list.controller.js
@@ -10,16 +10,38 @@
     function BlackListController($auth) {
         var vm = this;
 
+        vm.form = {
+            model: {}
+        };
+
+        vm.list = {
+            checked: [],
+            items: ['test@gmail.com']
+        };
+
         vm.add = add;
+        vm.remove = remove;
+
+        ////
 
         activate();
 
         function activate() {
             vm.user = $auth.user;
         }
+
+        function add(form) {
+            if (form.$invalid) return;
+            vm.list.items.push(vm.form.model.email);
+            vm.form.model.email = '';
+        }
         
-        function add() {
-            
+        function remove() {
+            _.forEach(vm.list.checked, function(checked) {
+                _.remove(vm.list.items, function(item) {
+                    return checked === item;
+                });
+            });
         }
     }
 })();
