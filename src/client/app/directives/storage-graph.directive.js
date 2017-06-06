5b87d6e02eca781ae472da77b54304b4afba6b0e
Aleksand Semenov <lovealldevelop@gmail.com>
2017-06-06 08:51:33 +0300

    Added tarif fix

--- src/client/app/directives/storage-graph.directive.js
+++ src/client/app/directives/storage-graph.directive.js
@@ -5,10 +5,10 @@
         .module('app.directives')
         .directive('storageGraph', storageGraph);
 
-    storageGraph.$inject = ['$auth', '$compile', '$timeout', '$templateRequest'];
+    storageGraph.$inject = ['$auth'];
 
     /* @ngInject */
-    function storageGraph($auth, $compile, $timeout, $templateRequest) {
+    function storageGraph($auth) {
         var directive = {
             link: link,
             restrict: 'A'
@@ -16,7 +16,6 @@
         return directive;
 
         function link(scope, element, attrs) {
-
             function $$(selector, context) {
                 context = context || document;
                 var elements = context.querySelectorAll(selector);
@@ -24,11 +23,9 @@
             }
 
             $$('.storage__space-pie--my-space').forEach(function (pie) {
-                var spaceDisk = $auth.user.profile.freeQuota;
+                var spaceDisk = ($auth.user.profile.usedQuota * 100 / $auth.user.profile.freeQuota);
                 pie.style.animationDelay = '-' + parseFloat(spaceDisk) + 's';
             });
-            console.log('eror');
-
         }
     }
 
