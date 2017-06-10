0a81ecc437cdaa797fd9bf82139f5cae0c9c14ec
Aleksandr Semenov <lovealldevelop@gmail.com>
2017-06-11 00:06:24 +0300

    Added stage commit

---
+++ src/client/app/directives/compose-minimize.directive.js
@@ -0,0 +1,31 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.directives')
+        .directive('composeMinimize', composeMinimize);
+
+    composeMinimize.$inject = ['$sce', '$compile', '$timeout', '$templateRequest'];
+
+    /* @ngInject */
+    function composeMinimize($sce, $compile, $timeout, $templateRequest) {
+        var directive = {
+            link: link,
+            restrict: 'A'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+
+            element.click(function (e) {
+                var $el = element.offsetParent().offsetParent().offsetParent();
+
+                $timeout(function () {
+                    angular.element($el).addClass('popup--compose-mini');
+                    $('.modal-backdrop').css('display', 'none');
+                }, 250);
+            });
+        }
+    }
+
+})();
