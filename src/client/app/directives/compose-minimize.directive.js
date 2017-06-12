540afe778fde5b8d82bc20e9ab7f16fda639dc93
Aleksand Semenov <lovealldevelop@gmail.com>
2017-06-12 09:10:39 +0300

    Added build

--- src/client/app/directives/compose-minimize.directive.js
+++ src/client/app/directives/compose-minimize.directive.js
@@ -17,14 +17,32 @@
 
         function link(scope, element, attrs) {
 
+            var isMinimize = false;
+
             element.click(function (e) {
                 var $el = element.offsetParent().offsetParent().offsetParent().offsetParent();
 
+                if (!isMinimize) {
+                    $timeout(function () {
+                        angular.element($el).addClass('popup--compose-mini');
+                        $('.modal-backdrop').css('display', 'none');
+                    }, 250);
+                    isMinimize = true;
+                    return;
+                }
+
                 $timeout(function () {
-                    angular.element($el).addClass('popup--compose-mini');
-                    $('.modal-backdrop').css('display', 'none');
+                    angular.element($el).removeClass('popup--compose-mini');
+                    $('.modal-backdrop').css('display', 'block');
+                    isMinimize = false;
                 }, 250);
             });
+            
+            function setPosition() {
+                $('.popup--compose-mini').forEach(function (e) {
+
+                });
+            }
         }
     }
 
