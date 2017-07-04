2e9e81bb3820b614c940fb979df04a356a889df8
Aleksand Semenov <lovealldevelop@gmail.com>
2017-07-04 15:12:12 +0300

    Added build

---
+++ src/client/app/directives/edit-inline.directive.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.directives')
+        .directive('editInline', editInline);
+
+    editInline.$inject = ['$timeout'];
+
+    /* @ngInject */
+    function editInline($timeout) {
+        var directive = {
+            link: link,
+            restrict: 'A',
+            scope: true
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            $timeout(function () {
+                element.attr('size', (element.val().length + 1));
+                element.bind("keyup", function(){
+                    element.attr('size', element.val().length + 1);
+                });
+            }, 10);
+        }
+    }
+
+})();
