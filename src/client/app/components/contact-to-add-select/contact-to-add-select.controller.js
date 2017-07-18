c7fd17366bfed9f79a9000894d1aa41fa4948452
Aleksand Semenov <lovealldevelop@gmail.com>
2017-07-18 21:13:02 +0300

    Added fix

--- src/client/app/components/contact-to-add-select/contact-to-add-select.controller.js
+++ src/client/app/components/contact-to-add-select/contact-to-add-select.controller.js
@@ -19,6 +19,7 @@
         vm.getContacts = getContacts;
         vm.findContacts = findContacts;
         vm.openContactToAddPopup = openContactToAddPopup;
+        vm.onTagAdding = onTagAdding;
 
         ////
 
@@ -93,6 +94,13 @@
             modalInstance.result.then(function (response) {
                 vm.addresses = response;
             });
+        }
+        
+        function onTagAdding($tag) {
+            $tag.id = _.uniqueId("tag-");
+
+            console.log('$tag', $tag);
+            return $tag;
         }
     }
 })();
