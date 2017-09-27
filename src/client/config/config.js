cf4643aae34a8774af90bd5374df7a7e1549837c
Aleksand Semenov <lovealldevelop@gmail.com>
2017-09-27 12:11:18 +0300

    Added config parent host

--- src/client/config/config.js
+++ src/client/config/config.js
@@ -3,6 +3,7 @@
 
     window.appConfig = {
         env: 'sandbox',
+        parentHost: 'https://mail.cz',
         APIHost: 'http://apimail.devogic.com',
         MediaUrl: 'http://apimail.devogic.com/',
         AttachUrl: 'http://apimail.devogic.com/mail/',
