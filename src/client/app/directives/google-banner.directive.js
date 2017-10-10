ffc8e5c457aeeb9d81f33ad9433b81fd0dde8fb6
Aleksand Semenov <lovealldevelop@gmail.com>
2017-10-10 19:32:26 +0300

    Added build

--- src/client/app/directives/google-banner.directive.js
+++ src/client/app/directives/google-banner.directive.js
@@ -5,19 +5,24 @@
         .module('app.directives')
         .directive('googleBanner', googleBanner);
 
-    googleBanner.$inject = [];
+    googleBanner.$inject = ['$timeout'];
 
-    function googleBanner() {
+    function googleBanner($timeout) {
         var directive = {
             link: link,
-            restrict: 'AE',
-            replace: true,
-            template: '<ins class="adsbygoogle" style="display:block;width:160px;height:600px" data-ad-client="a-pub-7169913763254428" data-ad-slot="6851262006" data-ad-format="auto"></ins>'
+            restrict: 'A'
         };
         return directive;
 
         function link(scope, element, attrs, form) {
-            (adsbygoogle = window.adsbygoogle || []).push({});
+            return $timeout(function() {
+                console.log('set google banner');
+                var adsbygoogle, html, rand;
+                rand = Math.random();
+                html = '<ins class="adsbygoogle" style="display:block;width:160px;height:600px" data-ad-client="a-pub-7169913763254428" data-ad-slot="6851262006" data-ad-format="auto"></ins>';
+                $(element).append(html);
+                return (adsbygoogle = window.adsbygoogle || []).push({});
+            });
         }
     }
 
