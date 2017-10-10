e1a119986d67399cd72cb8070978cd8b7e29499d
Aleksand Semenov <lovealldevelop@gmail.com>
2017-10-10 19:46:46 +0300

    Added build

--- src/client/app/directives/google-banner.directive.js
+++ src/client/app/directives/google-banner.directive.js
@@ -19,7 +19,7 @@
                 console.log('set google banner');
                 var adsbygoogle, html, rand;
                 rand = Math.random();
-                html = '<ins class="adsbygoogle" style="display:block;width:160px;height:600px" data-ad-client="a-pub-7169913763254428" data-ad-slot="6851262006" data-ad-format="auto"></ins>';
+                html = '<ins class="adsbygoogle" style="display:block;width:320px;height:108px" data-ad-client="a-pub-7169913763254428" data-ad-slot="6851262006" data-ad-format="auto" google-ad-width="200" google-ad-height="200"></ins>';
                 $(element).append(html);
                 return (adsbygoogle = window.adsbygoogle || []).push({});
             });
