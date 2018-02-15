88a05516dd6ab5584009e38de54056b593623b88
Aleksandr Semenov <iloveall@MacBook-Pro-Aleksandr.local>
2018-02-15 16:33:08 +0200

    Added translates to config

--- src/client/config/config.js
+++ src/client/config/config.js
@@ -9,6 +9,7 @@
         MediaUrl: 'https://mailcz.dev.mail.group/',
         AttachUrl: 'https://mailcz.dev.mail.group/mail/',
         passportLink: 'https://passport.wwwmailcz.dev.mail.group',
+        translatesUrl: 'https://mailadmin.mail.group',
         payment: {
             sandbox: 'AQgVJbIVhUkthElCdiLS1GZj7tq38wDeBspxYrLG_K73OzXL2_NTvBsQTDT9oXhEMTGQIiEtr8jPnFzF',
             production: 'AVZhosFzrnZ5Mf3tiOxAD0M6NHv8pcB2IFNHAfp_h69mmbd-LElFYkJUSII3Y0FPbm7S7lxBuqWImLbl'
